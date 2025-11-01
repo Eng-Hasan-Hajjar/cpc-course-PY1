94e86a62f38634ba93a6e0863bd91d6945e135df
Hasan Hajjar <hasanha@cpc.com>
2025-11-01 18:21:55 +0300

    lesson16

---
+++ lesson16.py
@@ -0,0 +1,209 @@
+# lesson16
+##quiz
+i=0
+while i<100:
+    i+=2
+    if i < 60 and i>40:
+        continue
+    print(i)    
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+   print(x)
+
+for x in "banana":
+     print(x)
+
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+ print(x)
+ if x == "banana":
+    break    
+ 
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+ if x == "banana":
+   break
+ print(x) 
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+ if x == "banana":
+  continue
+ print(x)
+ 
+
+for x in range(6):
+ print(x) 
+
+for x in range(2, 6):
+ print(x)
+
+
+for x in range(1,6,2):
+ print(x)
+
+for x in range(6):
+ print(x)
+else:
+ print("Finally finished!")
+
+
+
+
+
+for x in range(6):
+ if x == 3: break
+ print(x)
+else:
+ print("Finally finished!")
+
+
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+ for y in fruits:
+    print(x, y)
+
+
+for x in [0, 1, 2]:
+ pass
+
+def sum(a,b):
+  z=a+b
+  print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
